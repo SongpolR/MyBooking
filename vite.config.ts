8a94fbb77f29f6d9683b9d9c258f81e8c034b655
SongpolR <songpol.rungsawang@gmail.com>
2024-09-09 23:25:38 +0700

    chore: push vite config file.

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), basicSsl()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  base: '/MyBooking/',
+  server: {
+    port: 8090
   }
 })
